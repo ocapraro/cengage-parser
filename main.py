5f1ed39f0272770acb7969750e71d35e217ab144
Oscar Capraro <oscar@mcapraro.com>
2022-09-26 16:54:21 -0400

    fix:rectangle meddling

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def strip_code(code:str)->list:
   Returns:
   list: a list of all the character values.
   '''
-  x = re.findall('href="[^-]*-([^"]*)"|<\/rect>(.*)<\/g><[^\/]', code)
+  x = re.findall('href="[^-]*-([^"]*)"|<\/rect><g(.*)<\/g><[^\/]|<\/rect><use([^\/]*)<\/use><[^\/]', code)
   char_values = []
   for i in x:
     if i[0]:
       char_values.append(i[0])
-    else:
-      char_values.extend(strip_code(i[1]))
+    elif i[1] or i[2]:
+      char_values.extend(strip_code(i[1] or i[2]))
       char_values.append("rect")
   return char_values
 
@@ -86,8 +86,8 @@ def common_sense(equation:str)->str:
 def main():
   # code = input("paste mathjax here: ")
   code = """
-<svg xmlns:xlink="http://www.w3.org/1999/xlink" width="14.618ex" height="2.742ex" viewBox="0 -911.2 6294 1180.7" role="img" focusable="false" style="vertical-align: -0.626ex;"><g stroke="currentColor" fill="currentColor" stroke-width="0" transform="matrix(1 0 0 -1 0 0)"><use xlink:href="#MJMATHI-79" x="0" y="0"></use><use xlink:href="#MJMAIN-3D" x="778" y="0"></use><g transform="translate(1837,0)"><use xlink:href="#MJMATHI-78" x="0" y="0"></use><use transform="scale(0.707)" xlink:href="#MJMAIN-32" x="813" y="513"></use></g><use xlink:href="#MJMAIN-2B" x="3091" y="0"></use><g transform="translate(4095,0)"><use xlink:href="#MJMAIN-63"></use><use xlink:href="#MJMAIN-6F" x="447" y="0"></use><use xlink:href="#MJMAIN-74" x="951" y="0"></use></g><use xlink:href="#MJMATHI-78" x="5718" y="0"></use></g></svg>
-"""
+<svg xmlns:xlink="http://www.w3.org/1999/xlink" width="25.992ex" height="5.227ex" viewBox="0 -1446.1 11190.9 2250.4" role="img" focusable="false" style="vertical-align: -1.868ex;"><g stroke="currentColor" fill="currentColor" stroke-width="0" transform="matrix(1 0 0 -1 0 0)"><g transform="translate(120,0)"><rect stroke="none" width="1219" height="60" x="0" y="220"></rect><use xlink:href="#MJMATHI-64" x="346" y="676"></use><g transform="translate(60,-686)"><use xlink:href="#MJMATHI-64"></use><use xlink:href="#MJMATHI-78" x="523" y="0"></use></g></g><g transform="translate(1459,0)"><use xlink:href="#MJMAIN-28" x="0" y="0"></use><g transform="translate(392,0)"><use xlink:href="#MJMAIN-63"></use><use xlink:href="#MJMAIN-73" x="447" y="0"></use><use xlink:href="#MJMAIN-63" x="845" y="0"></use><use xlink:href="#MJMATHI-78" x="1572" y="0"></use></g><use xlink:href="#MJMAIN-29" x="2540" y="0"></use></g><use xlink:href="#MJMAIN-3D" x="4669" y="0"></use><use xlink:href="#MJMAIN-2212" x="5729" y="0"></use><g transform="translate(6677,0)"><use xlink:href="#MJMAIN-63"></use><use xlink:href="#MJMAIN-73" x="447" y="0"></use><use xlink:href="#MJMAIN-63" x="845" y="0"></use></g><use xlink:href="#MJMATHI-78" x="8249" y="0"></use><g transform="translate(8991,0)"><use xlink:href="#MJMAIN-63"></use><use xlink:href="#MJMAIN-6F" x="447" y="0"></use><use xlink:href="#MJMAIN-74" x="951" y="0"></use></g><use xlink:href="#MJMATHI-78" x="10615" y="0"></use></g></svg>
+  """
   stripped_code = strip_code(code)
   # print(stripped_code)
   translated = translate(stripped_code)
