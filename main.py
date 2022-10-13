b7f0dde8477dedb0a6a6b6ec81c65861359343aa
Oscar Capraro <oscar@mcapraro.com>
2022-10-13 16:57:28 -0400

    feat: add more common sense

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ def strip_code(code:str)->list:
     if i[0]:
       char_values.append(i[0])
     elif i[1] or i[2]:
+      char_values.append("{")
       char_values.extend(strip_code(i[1] or i[2]))
+      char_values.append("}")
       char_values.append("rect")
   return char_values
 
@@ -77,8 +79,10 @@ def common_sense(equation:str)->str:
   Returns:
   str: the modified equation.
   '''
-  for match in re.findall('([a-z])([0-9])',equation):
-    equation = re.sub('([a-z])([0-9])',match[0]+"^"+match[1],equation,1)
+  for match in re.findall('([a-n|p-z])([0-9])',equation):
+    equation = re.sub('([a-n|p-z])([0-9])',match[0]+"^"+match[1],equation,1)
+  for match in re.findall('lim([a-z])\\\\to(0|\\\\infty)',equation):
+    equation = re.sub('lim([a-z])\\\\to(0|\\\\infty)',"{"+match[0]+"\\\\lim"+match[1]+"}",equation,1)
   return equation
   
 
@@ -86,7 +90,7 @@ def common_sense(equation:str)->str:
 def main():
   # code = input("paste mathjax here: ")
   code = """
-<g stroke="currentColor" fill="currentColor" stroke-width="0" transform="matrix(1 0 0 -1 0 0)"><g transform="translate(120,0)"><rect stroke="none" width="12684" height="60" x="0" y="220"></rect><g transform="translate(60,676)"><use xlink:href="#MJMAIN-2212" x="0" y="0"></use><g transform="translate(781,0)"><use xlink:href="#MJMATHI-3B8" x="0" y="0"></use><use transform="scale(0.707)" xlink:href="#MJMAIN-32" x="668" y="513"></use></g><g transform="translate(1990,0)"><use xlink:href="#MJMAIN-73"></use><use xlink:href="#MJMAIN-69" x="397" y="0"></use><use xlink:href="#MJMAIN-6E" x="679" y="0"></use></g><use xlink:href="#MJMATHI-3B8" x="3508" y="0"></use><use xlink:href="#MJMAIN-2212" x="4203" y="0"></use><use xlink:href="#MJMAIN-32" x="5206" y="0"></use><use xlink:href="#MJMATHI-3B8" x="5710" y="0"></use><g transform="translate(6462,0)"><use xlink:href="#MJMAIN-63"></use><use xlink:href="#MJMAIN-6F" x="447" y="0"></use><use xlink:href="#MJMAIN-73" x="951" y="0"></use></g><use xlink:href="#MJMATHI-3B8" x="8091" y="0"></use><use xlink:href="#MJMAIN-2B" x="8786" y="0"></use><use xlink:href="#MJMAIN-32" x="9789" y="0"></use><g transform="translate(10573,0)"><use xlink:href="#MJMAIN-73"></use><use xlink:href="#MJMAIN-69" x="397" y="0"></use><use xlink:href="#MJMAIN-6E" x="679" y="0"></use></g><use xlink:href="#MJMATHI-3B8" x="12091" y="0"></use></g><g transform="translate(5877,-738)"><use xlink:href="#MJMATHI-3B8" x="0" y="0"></use><use transform="scale(0.707)" xlink:href="#MJMAIN-33" x="668" y="408"></use></g></g></g>
+<span class="equation" id="JWNUDMVM45J4QVZGA551" id-sequence="545"> <div><span class="MathJax_Preview" style="display: none;"></span><div class="MathJax_SVG_Display"><span class="MathJax_SVG" id="MathJax-Element-83-Frame" tabindex="0" style="font-size: 85%; display: inline-block;"><svg xmlns:xlink="http://www.w3.org/1999/xlink" width="13.294ex" height="6.083ex" viewBox="0 -1670.1 5723.6 2618.9" role="img" focusable="false" style="vertical-align: -2.204ex;"><g stroke="currentColor" fill="currentColor" stroke-width="0" transform="matrix(1 0 0 -1 0 0)"><g transform="translate(134,0)"><use xlink:href="#MJMAIN-6C"></use><use xlink:href="#MJMAIN-69" x="281" y="0"></use><use xlink:href="#MJMAIN-6D" x="563" y="0"></use></g><g transform="translate(0,-735)"><use transform="scale(0.8)" xlink:href="#MJMATHI-68" x="0" y="0"></use><use transform="scale(0.8)" xlink:href="#MJMAIN-2192" x="579" y="0"></use><use transform="scale(0.8)" xlink:href="#MJMAIN-30" x="1583" y="0"></use></g><g transform="translate(1669,0)"><g transform="translate(286,0)"><rect stroke="none" width="3647" height="60" x="0" y="220"></rect><g transform="translate(60,676)"><use xlink:href="#MJMAIN-32"></use><use xlink:href="#MJMAIN-2E" x="503" y="0"></use><use xlink:href="#MJMAIN-37" x="785" y="0"></use><use transform="scale(0.707)" xlink:href="#MJMATHI-68" x="1822" y="579"></use><use xlink:href="#MJMAIN-2212" x="2020" y="0"></use><use xlink:href="#MJMAIN-31" x="3024" y="0"></use></g><use xlink:href="#MJMATHI-68" x="1534" y="-686"></use></g></g></g></svg></span></div><script type="math/mml" id="MathJax-Element-83"><m:math xmlns:m="http://www.w3.org/1998/Math/MathML" display="block"> <m:mrow> <m:munder> <m:mi mathvariant="normal">lim</m:mi> <m:mstyle scriptsizemultiplier="0.8"> <m:mrow> <m:mi>h</m:mi> <m:mo>→</m:mo> <m:mn>0</m:mn></m:mrow></m:mstyle></m:munder> <m:mfrac> <m:mrow> <m:msup> <m:mn>2.7</m:mn> <m:mi>h</m:mi></m:msup> <m:mo>-</m:mo> <m:mn>1</m:mn></m:mrow> <m:mi>h</m:mi></m:mfrac></m:mrow></m:math></script></div> <button>TEST TEXT</button></span>
   """
   stripped_code = strip_code(code)
   # print(stripped_code)
